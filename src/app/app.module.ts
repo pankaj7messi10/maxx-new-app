ff5aa519eff0a7d7fbb22e0048c38b60eebc5958
Pachangrikar <pankaj.pachangrikar@worldline.com>
2024-02-18 00:12:13 +0530

    removing unused components from app.module ts declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { SuccessComponent } from './success/success.component';
-import { WarningComponent } from './warning/warning.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningComponent,
-    SuccessComponent
+    ServersComponent
   ],
   imports: [
     BrowserModule,
